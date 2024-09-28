48afb6f3c867332af8abe63cfb52335eea89703e
georgerabus <george.rabus@isa.utm.md>
2024-09-28 23:43:17 +0300

    files reading

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from openai import OpenAI
 client = OpenAI()
 
+path1 = "./data/0a866589-0d5d-4a9f-9121-89530653f42c.json"
+
+
+
+with open(path1, 'r') as file:
+    file_contents = file.read()
+
 
 completion = client.chat.completions.create(
     model="gpt-4o",
@@ -8,9 +15,11 @@ completion = client.chat.completions.create(
         {"role": "system", "content": "You are a helpful assistant."},
         {
             "role": "user",
-            "content": "Give me a list of all openai models"
+            "content": f"What do you see in this file {file_contents}"
         }
     ]
 )
+
+
 
 print(completion.choices[0].message)
